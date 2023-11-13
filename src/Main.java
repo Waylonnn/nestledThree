6fdd7e5c79264dfc511b6eb3643b0d3306b6d7ac
Waylon <weese.619547@swl.k12.oh.us>
2023-11-13 09:24:10 -0500

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double degreeF;
+        double degreeC;
+        int sent;
+        int sentt;
+        String cont;
+        Scanner scan = new Scanner(System.in);
+        do  {
+            sentt = 0;
+            sent = 0;
+            System.out.println("Enter A Degree Amount in Celsius:");
+            degreeC = scan.nextInt();
+            degreeF = (9/5) * degreeC + 32;
+            System.out.println("Degrees in Fahrenheit: " + degreeF);
+            while(sentt == 0){
+                System.out.println("Would you like to enter another degree? (Y/N to quit)");
+                cont = scan.nextLine();
+                if (cont.equalsIgnoreCase("N")) {
+                    sent = 1;
+                    sentt = 1;
+                } else if (cont.equalsIgnoreCase("Y")){
+                    sentt = 1;
+                } else{
+                    System.out.println("Invalid Input.");
+                }
+            }
+        } while (sent == 0);
     }
 }
